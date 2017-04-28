207db14ca97b786eeefd302d45796b3f45e2932b
Vlad Chichirau <you@example.com>
2017-04-29 01:18:47 +0300

    Added Google Maps API in new activity

--- app/src/main/java/com/example/vladut/subwayapp/MainActivity.java
+++ app/src/main/java/com/example/vladut/subwayapp/MainActivity.java
@@ -1,11 +1,13 @@
 package com.example.vladut.subwayapp;
 
+import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
@@ -35,19 +37,24 @@ public class MainActivity extends AppCompatActivity {
 
     MapView mapView;
 
+    public void bntHelpClick(View view){
+        Intent startNewActivity = new Intent(this, HelpActivity.class);
+        startActivity(startNewActivity);
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-//        mTextMessage = (TextView) findViewById(R.id.message);
-//        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
-//        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+        mTextMessage = (TextView) findViewById(R.id.message);
+        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
+        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
 
 
-        mapView = new MapView(this);
-        mapView.setBackgroundColor(Color.WHITE);
-        setContentView(mapView);
+//        mapView = new MapView(this);
+//        mapView.setBackgroundColor(Color.WHITE);
+//        setContentView(mapView);
     }
 
 }
